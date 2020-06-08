fb8c7f295852869923ac1000a25a13cff1fc3fc3
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-06-08 21:32:32 +0200

    LowerCase search phrase

--- components/Search.js
+++ components/Search.js
@@ -5,7 +5,15 @@ export default (state, actions) => h('div', { class: 'search' }, [
     type: 'text',
     class: 'search__input',
     value: state.searchPhrase,
-    oninput: (evt) => actions.setSearchPhrase(evt.target.value)
+    oninput: (evt) => {
+      const value = evt.target.value.trim().toLowerCase()
+      actions.setSearchPhrase(value)
+    },
+    onkeydown: (evt) => {
+      if (evt.key && evt.key === 'Escape') {
+        actions.setSearchPhrase('')
+      }
+    }
   }),
   !state.searchPhrase ? h('span', { class: 'search__suggestions' }, 'Sök bland dryckerna') : null
 ])
